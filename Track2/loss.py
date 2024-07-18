169fbe2b5a154408961c89a2923ca30c848c72c3
Tsai-chai-hsiang <tsaichiahsiang2000@gmail.com>
2024-07-19 04:02:21 +0800

    adding unit test

--- Track2/loss.py
+++ Track2/loss.py
@@ -14,3 +14,18 @@ class FocalLoss(nn.modules.loss._WeightedLoss):
         pt = torch.exp(-ce_loss)
         focal_loss = ((1 - pt) ** self.gamma * ce_loss).mean()
         return focal_loss
+
+if __name__ == "__main__":
+    batch_size = 10
+    max_length = 20
+    n_class = 5
+
+    weights = torch.FloatTensor([1, 1, 1, 1, 1])
+    criterion = FocalLoss(gamma=0.7, weights=weights)
+
+    m = torch.nn.Softmax(dim=-1)
+    logits = torch.randn(batch_size, max_length, n_class)
+    target = torch.randint(0, n_class, size=(batch_size, max_length))
+
+    loss = criterion(m(logits), target)
+    print(loss)
