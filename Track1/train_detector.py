e0315e5bb21808d61e5c4691a5fc42c9dbbbd54e
Tsai-chia-hsiang <tsaichiahsiang2000@gmail.com>
2024-07-19 23:20:53 +0800

    compelet resume() funciton

--- Track1/train_detector.py
+++ Track1/train_detector.py
@@ -35,6 +35,11 @@ def parse_cmd_args()->tuple[str, dict, dict]:
         print("="*indent)
         print(f"dataset     : {args['data_config']}")
         print(f"saving root : {args['project']}/{args['name']}/")
+        if args['resume']:
+            last_ckpt = Path(args['project'])/args['name']/"weights"/"last.pt"
+            assert os.path.exists(last_ckpt)
+            print(f"resume from {last_ckpt} -- {os.path.exists(last_ckpt)}")
+        
         print("="*indent)
         print()
         print(f"training hyperparameters for {modelarch}")
@@ -65,7 +70,8 @@ def parse_cmd_args()->tuple[str, dict, dict]:
     #path 
     parser.add_argument("--project", type=Path, default="./ckpt")
     parser.add_argument("--data_config", type=Path, default="Track1/configs/track1.yaml")
-    
+    parser.add_argument("--name", type=str, default="")
+
     # hyperparameterss
     parser.add_argument("--epochs", type=int, default=50)
     parser.add_argument("--lr0", type=float, default=1e-2)
@@ -75,6 +81,7 @@ def parse_cmd_args()->tuple[str, dict, dict]:
     parser.add_argument("--no_deterministic", action='store_false')
     parser.add_argument("--imgsz",type=int, default=1280)
     parser.add_argument("--amp", action='store_true')
+    parser.add_argument("--resume", action='store_true')
 
     args = vars(parser.parse_args())
     
@@ -82,7 +89,12 @@ def parse_cmd_args()->tuple[str, dict, dict]:
     assert 'rtdetr' in model_arch or 'yolo' in model_arch
     
     path_args={k:v for k,v in args.items() if k in ["data_config", "project"]}
-    path_args['name'] = Path(model_arch).stem + "_" + Path(args["data_config"]).stem 
+    if args["name"] == "":
+        path_args['name'] = Path(model_arch).stem + "_" + Path(args["data_config"]).stem 
+    else:
+        path_args['name'] = args['name']
+
+    path_args['resume'] = args['resume']
 
     train_args = {
         "epochs":50, "patience":-1, "batch":5, "device":0, "lr0":1e-2,
@@ -121,7 +133,8 @@ def parse_cmd_args()->tuple[str, dict, dict]:
     
     check = confirm(
         modelarch=model_arch, args=path_args, 
-        train_args=train_args)
+        train_args=train_args
+    )
     
     if not check:
         print("Abort.")
@@ -130,6 +143,7 @@ def parse_cmd_args()->tuple[str, dict, dict]:
     return model_arch, path_args, train_args
     
 def resume(last_ckpt:str, arch:Literal["yolo", 'rtdetr']):
+    print(f"{arch} <- {last_ckpt}")
     ultra_model:Model = MODEL_MAP[arch](last_ckpt)
     ultra_model.train(resume=True)
 
@@ -173,6 +187,18 @@ epochs = 50, imgsz = 1280, batch = 5,
 
 def main():
     model_arch, args, train_args = parse_cmd_args()
+    ultra_model = None
+    if args['resume']:
+        ckpt = Path(args['project'])/args['name']/"weights"/"last.pt"
+        a = ""
+        if 'yolo' in model_arch:
+            a = 'yolo'
+        elif 'rtdetr' in model_arch:
+            a = 'rtdetr'
+        
+        resume(last_ckpt=ckpt, arch=a)    
+        return 
+    
     ultra_model = get_general_pretrained_ulra_model(arch=model_arch)
     train_ultra_model(
         ultra_model=ultra_model, 
