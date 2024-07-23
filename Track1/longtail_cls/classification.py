932715502bf1c5df654f17aa2be150346290f11f
Tsai-chia-hsiang <tsaichiahsiang2000@gmail.com>
2024-07-23 22:05:55 +0800

    combine warmup into train_model

--- Track1/longtail_cls/classification.py
+++ Track1/longtail_cls/classification.py
@@ -18,13 +18,13 @@ def layze_parse_arg():
     parser.add_argument("--model", type=str, default="vit")
     parser.add_argument("--root", type=Path, default=Path("./")/"crop")
     parser.add_argument("--ckpt", type=Path, default=Path("ckpt")/"cls")
-    parser.add_argument("--lr", type=float, default=1e-2)
+    parser.add_argument("--lr", type=float, default=1e-3)
     parser.add_argument("--batch_size", type=int, default=64)
     parser.add_argument("--epochs", type=int, default=50)
-    parser.add_argument("--warmup_epochs", type=int, default=2)
+    parser.add_argument("--valid_epochs", type=int, default=2)
+    parser.add_argument("--warmup_epochs", type=int, default=5)
     parser.add_argument("--device", type=int, default=0)
     parser.add_argument("--optimizer", type=str, default="adamw")
-    parser.add_argument("--feature_dim", type=int, default=128)
     parser.add_argument("--debug_iter", type=int, default=-1)
     parser.add_argument("--seed", type=int, default=891122)
     args = parser.parse_args()
@@ -33,7 +33,7 @@ def layze_parse_arg():
 def main():
     
     args = layze_parse_arg()
-    set_seed(891122)
+    set_seed(args.seed)
     Path(args.ckpt).mkdir(parents=True, exist_ok=True)
     logger = get_logger(name=__name__, file=args.ckpt/"training.log")
     remove_old_tf_evenfile(args.ckpt)
@@ -45,7 +45,8 @@ def main():
         f"cuda:{max(args.device, torch.cuda.device_count()-1)}"
         if args.device >= 0 else "cpu"
     )
-    logger.info(f"Using {dev}") 
+    logger.info(f"Using : {torch.cuda.get_device_properties(dev)}")
+    logger.info("") 
     logger.info(f"build training set from {Path(args.root)/'train'}")
     train_dataset = MC_ReID_Features_Dataset.build_dataset(root=Path(args.root)/"train")
     logger.info(f"training set class counts : ")
@@ -56,21 +57,15 @@ def main():
     logger.info(f"validation set class counts : ")
     logger.info(f"{valid_dataset.cls_count.tolist()}")
     logger.info(f"")
-    model = ViT_Cls_Constractive_Model(
-        ncls=train_dataset.ncls, 
-        fdim=args.feature_dim
-    )
-    scl = SCL(model.ncls, device=dev, fdim=model.fdim)
+    model = ViT_Cls_Constractive_Model(ncls=train_dataset.ncls)
+    
     board = SummaryWriter(args.ckpt)
     model.train_model(
         train_set=train_dataset, valid_set=valid_dataset, dev=dev, 
-        batch=args.batch_size, epochs=args.epochs, 
-        optimizer=args.optimizer, lr=args.lr, 
-        scl_loss=scl, contrastive_learning=True,
-        ckpt = args.ckpt/"cls_vit",
-        val_epochs = 1, 
-        logger=logger,board=board, 
-        debug=args.debug_iter       
+        batch=args.batch_size, epochs=args.epochs, warm_up=args.warmup_epochs,
+        optimizer=args.optimizer, lr=args.lr, contrastive_learning=True,
+        ckpt = args.ckpt/"cls_vit", val_epochs = args.valid_epochs, 
+        logger=logger, board=board, debug=args.debug_iter       
     )
     """
     logger.info("with contrastive learning ..")
